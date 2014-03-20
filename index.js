c4453aa5c8cc88372a63777490bb9ea6f1f974fc
Stephen Simpson <me@simpo.org>
2014-03-20 11:11:34 +0000

    Reworked to use the fin() method to run after all the thens have run and report fulfillment to the stack.

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
 'use strict';
 
-var aspect = require('meld');
 var Q = require('q');
 
 var QQ = function QQ() {
 	var deferred = Q.defer();
 	var promiseStack = [];
-	var resolveCalled = 0;
-	var resolveDone = 0;
+	var promiseDone = 0;
 	
 	var construct = {
 		add: function(promise) {
 			var stackObject = createStackObject(promise);
 			
-			aspect.around(promise, 'then', function(original) {
-				var resolved = original.args[0];
-				var rejected = original.args[(original.args[3] === undefined) ? 2 : 3];
-				
-				original.args[0] = function(){
-					resolveCalled++;
-					resolved.apply(resolved, arguments);
-					resolveDone++;
-					if (!stackObject.resolved) {
-						stackObject.resolved = true;
-						if (resolveDone >= resolveCalled) {
-							deferred.resolve();
-						}
+			promise.fin(function(){
+				if (!stackObject.resolved) {
+					stackObject.resolved = true;
+					promiseDone++;
+					if (promiseDone >= promiseStack.length) {
+						deferred.resolve();
 					}
-				};
-				
-				original.args[(original.args[3] === undefined) ? 2 : 3] = function(){
-					deferred.reject.apply(deferred.reject, arguments);;
-					rejected.apply(rejected, arguments);
-				};
-				
-				original.proceed(original.args[0],original.args[1],original.args[2]);
+				}
 			});
-		
+			
 			promiseStack.push(stackObject);
 		},
 		
